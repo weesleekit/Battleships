d2a182d20486f58c0793ad133da83471eb226fce
weesleekit <weesleekit@sleekit.co.uk>
2022-09-02 20:18:59 +0100

    Reorganised tests

--- Battleships.Test/ShipBusinessRules.cs
+++ Battleships.Test/ShipBusinessRules.cs
@@ -6,31 +6,6 @@ namespace Battleships.Test
 {
     public class ShipBusinessRules
     {
-        [Theory]
-        // Testing -1
-        [InlineData("-1:2,3:5")]
-        [InlineData("3:-1,3:5")]
-        [InlineData("3:2,-1:5")]
-        [InlineData("3:2,3:-1")]
-        // Testing 10
-        [InlineData("10:2,3:5")]
-        [InlineData("3:10,3:5")]
-        [InlineData("3:2,10:5")]
-        [InlineData("3:2,3:10")]
-        public void Battleships_OutOfBoundsShipInput_ThrowsArgumentException(string input)
-        {
-            // Arrange
-            string[] ships = new[] { input };
-            string[] guesses = new[] { "7:0", "3:3" };
-
-            // Act
-            Action act = () => Game.Play(ships, guesses);
-
-            // Assert
-            act.Should().Throw<ArgumentException>();
-        }
-
-
         [Fact]
         public void Battleships_ShipStartsAndEndsSameSpace_ThrowsArgumentException()
         {
